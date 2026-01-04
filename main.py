1632e5cf792f0c9e9b3fea182c29d8a03ec5dbf2
Zanehisako <yassinetakiko@gmail.com>
2026-01-04 08:40:48 +0100

    added some new code that selectes the best features to keep

--- main.py
+++ main.py
@@ -1699,8 +1699,8 @@ async def main():
         # Load the input file containing club stats pages
         # Ensure you have a file named 'club_urls.csv' with a column 'club_url'
         # club_urls = pd.read_csv("club_urls_test.csv")['club_url'].tolist()
-        club_urls = pd.read_csv("club_urls_laliga.csv")['club_url'].tolist()
-        # club_urls = pd.read_csv("club_urls_premierLeague.csv")['club_url'].tolist()
+        # club_urls = pd.read_csv("club_urls_laliga.csv")['club_url'].tolist()
+        club_urls = pd.read_csv("club_urls_premierLeague.csv")['club_url'].tolist()
         # club_urls = pd.read_csv("club_urls_bundesliga.csv")['club_url'].tolist()
         # club_urls = pd.read_csv("club_urls_ligue1.csv")['club_url'].tolist()
         # clubs_urls = pd.read_csv("club_urls_serieA.csv")['club_url'].tolist()
